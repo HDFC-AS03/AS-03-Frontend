de0a0180e6f832aa5fceb5cff7fdce13a9569c5a
JOYNUR RAHMAN <143766169+Joynur-Rahman@users.noreply.github.com>
2026-03-09 17:51:58 +0530

    user-fetch-fixed

--- src/pages/UserManagement.jsx
+++ src/pages/UserManagement.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useRef, useEffect } from "react";
 
-const API_BASE = "http://localhost:8000";
+const API_BASE = "http://localhost";
 
 const ROLE_MAP = {
   Manager: "manager",
